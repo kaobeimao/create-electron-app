b261f99c657a544574f14ce0189f84c687ff3eba
lkd <479852731@qq.com>
2021-01-06 13:53:27 +0800

    #

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    })
+
+    win.loadFile('./app/index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
